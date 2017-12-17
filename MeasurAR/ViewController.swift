d94340c4d2fa061874ed111576672ac1547f75dc
yang968 <yang968@purdue.edu>
2017-12-17 02:08:33 -0500

    Add Restart button in UI and SCNNode implementations to place spheres where user touches on screen

--- MeasurAR/ViewController.swift
+++ MeasurAR/ViewController.swift
@@ -10,21 +10,89 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet weak var sceneView: ARSCNView!
-    @IBOutlet weak var displayLabel: UIView!
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var restartButton: UIButton!
+    
+    var dotNodes = [SCNNode]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sceneView.delegate = self
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /*
+        if dotNodes.count >= 2 {
+            dotNodes.forEach({ $0.removeFromParentNode() })
+            sceneView.isUserInteractionEnabled = false
+        }*/
+        switch dotNodes.count {
+        case 0:
+            displayLabel.text = "Touch Screen to place 1st point"
+        case 1:
+            displayLabel.text = "Now place 2nd point"
+        default:
+            sceneView.isUserInteractionEnabled = false
+        }
+        
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResults.first {
+                showDot(at: hitResult)
+            }
+        }
+    }
+    
+    func showDot(at hitResult: ARHitTestResult) {
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        dotGeometry.materials = [material]
+        
+        let node = SCNNode()
+        node.geometry = dotGeometry
+        node.position = SCNVector3(
+            hitResult.worldTransform.columns.3.x,
+            hitResult.worldTransform.columns.3.y,
+            hitResult.worldTransform.columns.3.z )
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        dotNodes.append(node)
+        
+        if dotNodes.count >= 2 {
+            calculateDistance()
+            restartButton.isHidden = false
+        }
+    }
+    
+    func calculateDistance() {
+        
+    }
+    
+    @IBAction func restartPressed(_ sender: Any) {
+        dotNodes.forEach({ $0.removeFromParentNode() })
+        restartButton.isHidden = true
+        sceneView.isUserInteractionEnabled = true
+    }
+    
 }
 
