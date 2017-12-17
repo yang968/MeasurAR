ddd598a59a45910f8c3d672da182e62a0479f162
yang968 <yang968@purdue.edu>
2017-12-17 01:02:01 -0500

    Initial Commit

---
+++ MeasurAR/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MeasurAR
+//
+//  Created by Spencer Yang on 12/16/17.
+//  Copyright © 2017 Seungho Yang. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var displayLabel: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
